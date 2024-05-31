2e2b274cda340c651a4b1b1c4d8d7c19fbc9e82c
nguyetntmse161788 <nguyetthm02@gmail.com>
2024-05-31 07:15:19 +0700

    crud product

--- Exe201_backend/Program.cs
+++ Exe201_backend/Program.cs
@@ -1,10 +1,10 @@
 using Exe201_backend;
-
 using Service.Service.System.Authen;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Service.Repo;
 using Service.Models;
+using Service.Service.System.Product;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,8 +16,13 @@ builder.Services.AddDatabase();
 builder.Services.AddTransient<SignInManager<User>, SignInManager<User>>();*/
 
 builder.Services.AddTransient<IAuthenService, AuthenService>();
-
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllers();
+builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+            });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Service/ViewModel/System/CreateProductVariantDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.ViewModel.System
+{
+
+    public class CreateProductVariantDTO
+    {
+        public string? SizeName { get; set; }
+        public string? BrandName { get; set; }
+        public string? ColorName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
